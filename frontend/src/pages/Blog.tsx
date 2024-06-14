44c6a2de89ff0992d437e5edf6f61d69f0d1a978
Nikhil1473 <2021uee1473@mnit.ac.in>
2024-06-14 16:34:24 +0530

    Improvement in frontend

--- frontend/src/pages/Blog.tsx
+++ frontend/src/pages/Blog.tsx
@@ -1,26 +1,30 @@
-import { useBlog } from "../hooks";
 import { useParams } from "react-router-dom";
 import { FullBlog } from "../components/FullBlog";
 import { Spinner } from "../components/Spinner";
-import { Appbar } from "../components/Appbar"; // Ensure Appbar is imported
+import { Appbar } from "../components/Appbar";
+import { useBlog } from "../hooks/index";
 
+export const Blog = () => {
+    const { id } = useParams();
+    const { loading, blog } = useBlog({ id: id || "" });
 
-export default function Blog(){
-    const {id} = useParams();
-    const {loading, blog} = useBlog(
-       { id: id || ""}
-    );
-
-    if(loading || !blog){
-        return(
+    if (loading || !blog) {
+        return (
             <div>
-                <Spinner />
+                <Appbar />
+                <div className="h-screen flex flex-col justify-center">
+                    <div className="flex justify-center">
+                        <Spinner />
+                    </div>
+                </div>
             </div>
-        )
+        );
     }
-    return(
+
+    return (
         <div>
-            <FullBlog blog={blog } /> 
+            <Appbar />
+            <FullBlog blog={blog} />
         </div>
-    )
-}
+    );
+};
